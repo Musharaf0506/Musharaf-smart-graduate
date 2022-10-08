d8e63370a67508df3ab3e61905d47fd99b3582ac
Musharaf <113353458+Musharaf0506@users.noreply.github.com>
2022-10-08 08:37:10 +0530

    Add files via upload

---
+++ open-lab-book-java-1/PrintANumber.java
@@ -0,0 +1,9 @@
+ import java.util.scanner;
+public class PrintANumber{
+   public static void main (String[] args){
+  Scanner reader = new Scanner(System.in);
+  System.out.println("enter a number:");
+int number =reader.nextInt();
+   System.out.println("you entered:"+number);
+   }
+}
